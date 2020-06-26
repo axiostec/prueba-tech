8f291b27d8aa2e018cb3a349fb4715642f32a051
axiostec <contacto@axiostec.com>
2020-06-26 04:50:56 -0500

    [add] deploy

---
+++ app/precache-manifest.7c35b0307dcbd7c275c01efe330a6707.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ea6167493f16d83e8224a005bd1df69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f9e1a816b98af18d727",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0f9e1a816b98af18d727",
+    "url": "/static/js/2.75202428.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.75202428.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bda9762db160451851e8",
+    "url": "/static/js/main.a633edbc.chunk.js"
+  },
+  {
+    "revision": "e072d11fc3fec6dabe11",
+    "url": "/static/js/runtime-main.8cef47f5.js"
+  }
+]);
