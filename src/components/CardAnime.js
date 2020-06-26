182b7440fbffabc2e8a0191798161966d5f512c4
axiostec <contacto@axiostec.com>
2020-06-26 01:59:34 -0500

    [add] adjust css card anime

--- src/components/CardAnime.js
+++ src/components/CardAnime.js
@@ -7,8 +7,8 @@ export default class CardAnime extends Component{
             <Card key={this.props.mal_id} className="CardAnime">
                 <Card.Img variant="top" src={this.props.image_url} />
                 <Card.Body className="d-flex flex-column justify-content-between">
-                    <Card.Title>{this.props.title}</Card.Title>
-                    <Card.Text>
+                    <Card.Title className="h6">{this.props.title}</Card.Title>
+                    <Card.Text className="textSinopsis">
                     {this.props.synopsis}
                     </Card.Text>
                     <Button variant="primary">Add Favorite</Button>
--- src/css/ContData.js
+++ src/css/ContData.js
@@ -2,16 +2,21 @@ import styled from 'styled-components';
 
 const ContAnime = styled.div`
     .CardAnime{
-        width: 20rem;
+        width: 13rem;
+        border-radius: 10px;
+        overflow: hidden;
         margin-right: 5px;
         margin-bottom: 5px;
+    }
+    .textSinopsis{
+        font-size: .7em;
     }
     small{
         font-weight: 300;
     }
     img{
         width: 100%;
-        height: 500px;
+        height: 200px;
         object-fit: cover;
     }
 `;
